d3496f7b7933c3625bc01a4e89d62ffd89bbce8d
Nadine Curry <nlcurry@gmail.com>
2016-05-18 23:04:39 -0700

    methods for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    @user ||= User.find(id: session[:user_id])
+  end
+
+  def require_login
+    if current_user.nil?
+      flash[:error] = "You must be logged in to view"
+      redirect to root_path
+  end
 end

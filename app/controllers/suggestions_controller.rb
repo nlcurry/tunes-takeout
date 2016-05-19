4f35cf5a7e17f63df035a13733226f3529783ae3
Nadine Curry <nlcurry@gmail.com>
2016-05-18 23:05:04 -0700

    Setting up controllers and routes

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,2 +1,19 @@
 class SuggestionsController < ApplicationController
+  skip_before_action :require_login, only: :index
+
+  def index
+
+  end
+
+  def favorites
+
+  end
+
+  def favorite
+
+  end
+
+  def unfavorite
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root 'suggestions#index'
+
+  post '/suggestions' => 'suggestions#show', as: 'search'
   resources :users,    :only => [:new, :create]
   resources :sessions, :only => [:create]
-  delete "/logout" => "sessions#destroy"
+
+
+  delete "/logout" => "sessions#destroy", as: 'logout'
   get    "/auth/:provider/callback"  => "sessions#create"
   # delete "/logout" => "sessions#destroy"
   # get    "/login"  => "sessions#new"

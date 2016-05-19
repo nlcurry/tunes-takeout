eb4508c79405bbcd33c76ad2bbc8ee0e51f99f8f
Nadine Curry <nlcurry@gmail.com>
2016-05-18 23:05:44 -0700

    added omniauth code and schema changes

---
+++ db/migrate/20160519055803_deleting_null_restrictions.rb
@@ -0,0 +1,6 @@
+class DeletingNullRestrictions < ActiveRecord::Migration
+  def change
+    change_column :users, :uid, :string, null: true
+    change_column :users, :provider, :string, null: true
+  end
+end
---
+++ db/migrate/20160519060114_deleteemailcolumn.rb
@@ -0,0 +1,5 @@
+class Deleteemailcolumn < ActiveRecord::Migration
+  def change
+    remove_column :users, :email
+  end
+end

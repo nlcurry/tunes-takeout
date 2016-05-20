9b2468c07451619c9109666d92bd84b12c6feb42
Nadine Curry <nlcurry@gmail.com>
2016-05-20 12:59:43 -0700

    fixed url for wrapper and got suggestion information to pass through index

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,10 +1,16 @@
+require_relative "../../lib/TunesTakeoutWrapper"
+
 class SuggestionsController < ApplicationController
   skip_before_action :require_login, only: :index
 
   def index
-    if session[:user_id]
+    if current_user
       @user = User.find(session[:user_id])
     end
+
+    @suggestion_ids = TunesTakeoutWrapper.top_suggestions
+    @suggestions = @suggestion_ids.map {|id| TunesTakeoutWrapper.retrieve(id)["suggestion"]}
+
   end
 
   def favorites
--- lib/TunesTakeoutWrapper.rb
+++ lib/TunesTakeoutWrapper.rb
@@ -8,14 +8,14 @@ class TunesTakeoutWrapper
     data = HTTParty.get(BASE_URL + "/v1/suggestions/search?query=#{query}&limit=#{limit}&seed=#{query}").parsed_response
   end
 
-  #find a defined list of top suggestions
+  #find a defined size of top suggestions, brings back an array
   def self.top_suggestions(limit=20)
     data = HTTParty.get(BASE_URL + "/v1/suggestions/top?limit=#{limit}").parsed_response["suggestions"]
   end
 
   #find suggestion attributes based on suggestion ID
   def self.retrieve(suggestion_id)
-    HTTParty.get(BASE_URL + "v1/suggestions/#{suggestion_id}").parsed_response
+    HTTParty.get(BASE_URL + "/v1/suggestions/#{suggestion_id}").parsed_response
   end
 
   def self.favorites(uid)
